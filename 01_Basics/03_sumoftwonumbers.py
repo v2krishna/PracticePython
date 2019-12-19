c97e1a47d274175f687ac2e856ae10a8a7667245
v2krishna <v2krishna@gmail.com>
2019-12-18 21:53:23 -0600

    adding notes about indentation and variables and assignment

---
+++ 01_Basics/03_sumoftwonumbers.py
@@ -0,0 +1,11 @@
+"""
+Create two variables , then find the sum of two numbers
+make sure the two numbers are integers or numbers
+"""
+
+n1 = int(input("Enter a integer n1: "))
+n2 = int(input("Enter a integer n2: "))
+
+
+result = n1+n2
+print("Sum of two numbers {} and {} = {}".format(n1, n2, result))
