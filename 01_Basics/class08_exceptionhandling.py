413fb96f875dd3f0da25e76b1cb85dd539ec3ac4
v2krishna <v2krishna@gmail.com>
2020-07-28 06:06:13 -0500

    adding notes about indentation and variables and assignment

---
+++ 01_Basics/class08_exceptionhandling.py
@@ -0,0 +1,14 @@
+"""
+Exception Handling
+==================
+Error -- when it occurs at compile time.
+Exception -- occurs at run-time
+
+try      -->  we write suspicious code within this statements or clause
+except   -->  this block executes only if there's exception occured in try block, single try block may have multiple except clauses
+              we can use except clause to handle individual or multiple exceptions at a time.
+else    -->   This block exceutes only if there's no exception occured in try block,
+              we can use only single else block per try block
+finally -->   This block executes anyhow that is may or may not error occured, it executes
+raise   -->   This statement raises an exception which can be handled by except clause.
+"""
