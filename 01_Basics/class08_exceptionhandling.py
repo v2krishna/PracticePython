0947ef64b663c7108f0b53ad142be6ee38ba3da3
v2krishna <v2krishna@gmail.com>
2020-07-28 06:40:24 -0500

    adding notes about indentation and variables and assignment

--- 01_Basics/class08_exceptionhandling.py
+++ 01_Basics/class08_exceptionhandling.py
@@ -11,4 +11,87 @@ else    -->   This block exceutes only if there's no exception occured in try bl
               we can use only single else block per try block
 finally -->   This block executes anyhow that is may or may not error occured, it executes
 raise   -->   This statement raises an exception which can be handled by except clause.
+
+l = [0, 0.7, 'high', 2]
+
+for i in l:
+    print("i is {}".format(i))
+    try:
+        c = 1/int(i)
+    except ZeroDivisionError as Z:
+        print(Z)
+    except ValueError as V:
+        print(V)
+    else:
+        print("This is else block")
+        print(c)
+    finally:
+        print("This is finally block")
+===============================================================
+n numbers average
+import sys
+
+tot= 0
+count=0
+while True:
+    num = input("Please enter number or No to break: ")
+    if num =='No':
+        break
+    else:
+        try:
+            tot += float(num)
+            count +=1
+        except:
+            print(sys.exc_info()[0])
+        else:
+            print("This is else block")
+        finally:
+            print("This is final block")
+avg = tot/count
+print("Average is {}".format(avg))
+===============================================================
+import sys
+def throw():
+    raise  RuntimeError("This is the error")
+try:
+    throw()
+except:
+    print(sys.exc_info()[0])
+
+===============================================================
+try:
+    a = 10/0
+    print(a)
+except:
+    print(sys.exc_info()[0])
+===============================================================
+import sys
+try:
+    a = int(input('a value:'))
+    b = int(input('b value:'))
+    assert a==b, 'Value mismatch'
+except:
+    print(sys.exc_info()[0])
+===============================================================
+import sys
+try:
+    print("Press return or control plus C")
+    ignore = input("Enter")
+except:
+    print(sys.exc_info()[0])
+===============================================================
+import sys
+try:
+    exec('1+10')
+except:
+    print(sys.exc_info()[0])
+===============================================================
+s = "abcd"
+it = iter(s)
+for i in range(5):
+    try:
+        print(next(it))
+    except StopIteration as st:
+        print('Stop Iteration')
+===============================================================
 """
