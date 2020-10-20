71aa523aaf498ad95d3f9726defa9494e3f6330d
v2krishna <v2krishna@gmail.com>
2020-10-19 22:57:20 -0500

    adding notes about indentation and variables and assignment

--- 01_Basics/class19_OOPS3.py
+++ 01_Basics/class19_OOPS3.py
@@ -1,2 +1,34 @@
 """
-Encapsuf"{self.a}\n{self._b}\n{self.__c}") k nnj;
+Encapsulation
+=============
+name = "public"
+_name = "protected"
+__name = "private"
+"""
+class Demo:
+    def __init__(self):
+        self.name = "public"
+        self._age = "protected"
+        self.__year = "private"
+
+    def display(self):
+        print(self.name)
+        print(self._age)
+        print(self.__year)
+
+    def __view(self):
+        print("This is private method")
+obj = Demo()
+obj.display()
+
+print('Acccessing from outside of the class')
+print(obj.name)
+print(obj._age)
+#print(obj.__year) # it will raise error
+
+
+#to access private variables /members of the class use 'object._classname__variablename'
+print(obj._Demo__year)
+
+#obj.__view()
+obj._Demo__view()
